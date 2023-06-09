b87399e0aa68a193e5e128daa45e4893680a04f5
Devam-Sheth <devamsheth1203@gmail.com>
2023-06-09 22:09:52 +0530

    updated

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
+var checkbox = document.querySelectorAll("input[type = 'checkbox']");
 var i,j;
-let checkboxValue = [];
+let checkboxValue = []; 
 
 document.addEventListener('keydown', function() {
-    document.querySelectorAll('[type ="checkbox"]').forEach(item => {
-        if(item.checked === true) {
-            checkboxValue.push(item.value);
+    checkbox.forEach(item => {
+        if (item.checked === true) {
+            checkboxValue.push(item.value); 
             i = checkboxValue[0];
             j = checkboxValue[checkboxValue.length-1];
-            console.log(i,j);
-            for (let k = i; k <= j; k++) {
+            for ( let k = i; k <= j; k++) {
                 document.getElementById(k).checked = true;
+            }           
+        } else {
+            m= item.value;
+            let firstCheckedBox = parseInt(i);
+            let lastCheckedBox = parseInt(j);
+            let uncheckedValues = parseInt(m);
+            let total = firstCheckedBox+lastCheckedBox;
+            if(uncheckedValues < 6  && total >= uncheckedValues && firstCheckedBox!==lastCheckedBox) {
+                console.log(firstCheckedBox,lastCheckedBox, uncheckedValues);
+                for (let f = i; f < m; f++) {
+                    console.log(document.getElementById(f));
+                    document.getElementById(f).checked = false;
+                    console.log(document.getElementById(f).checked);   
+                }
             }
+            console.log(document.getElementById(2).checked);
         }
-    });
-    
+    })
 })
